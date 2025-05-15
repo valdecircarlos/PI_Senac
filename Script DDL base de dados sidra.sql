e6f0bc7ce683226f9c5ead86de24407d32ef6e8a
valdecircarlos <130991922+valdecircarlos@users.noreply.github.com>
2025-05-15 19:46:42 -0300

    Update Script DDL base de dados sidra.sql

--- Script DDL base de dados sidra.sql
+++ Script DDL base de dados sidra.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE sidra;
 use sidra;
-CREATE TABLE pmc_8881 (
+CREATE TABLE pmc_8880 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     valor DECIMAL(15,2),
     indice VARCHAR(255),
@@ -8,7 +8,7 @@ CREATE TABLE pmc_8881 (
     mesano_codigo VARCHAR(10),
     mesano VARCHAR(100)
 );
-ALTER TABLE pmc_8881
+ALTER TABLE pmc_8880
 ADD UNIQUE KEY unique_registro (indice, uf, mesano_codigo);
 CREATE TABLE pmc_8881 LIKE pmc_8880;
 
