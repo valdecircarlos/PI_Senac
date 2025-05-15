4ef3e1d0b81782b9dc281fab26e17acd2ef3dd77
valdecircarlos <130991922+valdecircarlos@users.noreply.github.com>
2025-05-15 19:45:24 -0300

    Add files via upload

--- Script DDL base de dados sidra.sql
+++ Script DDL base de dados sidra.sql
@@ -1,5 +1,6 @@
+CREATE DATABASE sidra;
 use sidra;
-CREATE TABLE pmc_8880 (
+CREATE TABLE pmc_8881 (
     id INT AUTO_INCREMENT PRIMARY KEY,
     valor DECIMAL(15,2),
     indice VARCHAR(255),
@@ -7,6 +8,21 @@ CREATE TABLE pmc_8880 (
     mesano_codigo VARCHAR(10),
     mesano VARCHAR(100)
 );
-ALTER TABLE pmc_8880
+ALTER TABLE pmc_8881
 ADD UNIQUE KEY unique_registro (indice, uf, mesano_codigo);
-select * from pmc_8880
+CREATE TABLE pmc_8881 LIKE pmc_8880;
+
+CREATE TABLE pmc_8882 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    valor DECIMAL(15,2),
+    indice VARCHAR(255),
+    atividades VARCHAR(255),
+    uf VARCHAR(20),
+    mesano_codigo VARCHAR(10),
+    mesano VARCHAR(100)
+);
+ALTER TABLE pmc_8882
+ADD UNIQUE KEY unique_registro (indice, atividades, uf, mesano_codigo);
+CREATE TABLE pmc_8883 LIKE pmc_8882;
+
+select * from pmc_8883
